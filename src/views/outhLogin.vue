30d067c366e8437ca3c7445a2a5c08a1b4a28453
liaoyj <892471693@qq.com>
2021-02-22 00:50:24 +0800

    now commit

---
+++ src/views/outhLogin.vue
@@ -0,0 +1,34 @@
+<template>
+  <div style="background-color: #CCCCCC;" />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      res: this.$route.query.token
+    }
+  },
+  mounted() {
+    console.log('liston...')
+    console.log(this.res)
+    const loading = this.$loading({
+      lock: true,
+      text: '登陆中',
+      spinner: 'el-icon-loading',
+      background: 'rgba(0, 0, 0, 0.7)'
+    })
+    this.saveInfo()
+    loading.close()
+  },
+  methods: {
+    saveInfo() {
+      console.log(this.res)
+      window.opener.postMessage(this.res, '*')
+    }
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss">
+</style>
